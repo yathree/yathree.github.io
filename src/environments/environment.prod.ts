c32fc29feab93efe16e79291232f299d4a6ed766
yathree <yathreemohd@gmail.com>
2022-07-29 13:37:37 +0300

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+const baseUrl = 'http://102.223.7.34:8281';
+
 export const environment = {
-  production: true
+  production: true,
+  appTitle: 'Mamlaka ya usafiri na usalama barabarani',
+  logoTitle: 'ZODLAP',
+  baseUrl: `${baseUrl}/api`,
+  whitelistedDomains: [baseUrl, '102.223.7.34', '102.223.7.34:8281'],
+  blacklistedRoutes: [`${baseUrl}/api/signin`],
 };
